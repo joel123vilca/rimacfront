d72776b089d19c941f82db869a29f262718c1fe2
joel vilca Alania <joevil23tjq@gmail.com>
2024-01-26 12:06:37 -0500

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,37 @@ import Home from "./pages/Homepage";
 import Plans from "./pages/Plans";
 import Resumen from "./pages/Resumen";
 import Layout from "./shared/Layout";
-import { Provider } from "react-redux";
-import { store } from "./redux/reducers/userReducer";
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          />
-          <Route
-            path="/plans"
-            element={
-              <Layout>
-                <Plans />
-              </Layout>
-            }
-          />
-          <Route
-            path="/resumen"
-            element={
-              <Layout>
-                <Resumen />
-              </Layout>
-            }
-          />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/plans"
+          element={
+            <Layout>
+              <Plans />
+            </Layout>
+          }
+        />
+        <Route
+          path="/resumen"
+          element={
+            <Layout>
+              <Resumen />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
